e45993e35fc95965db3d08f2f98968d90c44114a
Chaiyanon3010 <chaiyanon.prom@bumail.net>
2024-03-19 09:41:36 +0700

    Update Script Rotate

--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializeField] Vector3 torque, angularV;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            RotateAngularV();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            RotateTorque();
+        }
+    }
+
+    private void RotateTorque()
+    {
+        rb.AddTorque(torque);
+    }
+
+    private void RotateAngularV()
+    {
+        rb.angularVelocity = angularV;
     }
 }
